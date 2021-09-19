d198954ac003484a4bff33aca1d637c5e637caef
Rommy Gustiawan <rgcppp@gmail.com>
2021-09-19 08:34:27 +0700

    Add simple 5x5 grid
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,12 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int[][] grid = {
+            {0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0}
+        };
     }
 }
