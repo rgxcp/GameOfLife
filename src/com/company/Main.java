1ac57c164c2d5137d9bfa203fa25244880ba2dc9
Rommy Gustiawan <rgcppp@gmail.com>
2021-09-19 09:04:40 +0700

    Check if cell has top left neighbor (#2)
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,9 +13,11 @@ public class Main {
         for (int row = 0; row < grid.length; row++) {
             for (int column = 0; column < grid.length; column++) {
                 System.out.println(grid[row][column]);
-                boolean cellHasTopLeftNeighbor = false;
-                if () {
-                    cellHasTopLeftNeighbor = true;
+                boolean cellHasTopLeftNeighbor = true;
+                // if current index: row = 0, column = 0
+                // top left neighbor index must be: row = -1, column = -1
+                if (row - 1 <= grid.length && column - 1 <= grid.length) {
+                    cellHasTopLeftNeighbor = false;
                 }
                 System.out.println("Cell has top left neighbor: " + cellHasTopLeftNeighbor);
             }
