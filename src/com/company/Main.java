8ad2628814d3b4fecaf71e6be12c40cc14a24523
Rommy Gustiawan <rgcppp@gmail.com>
2021-09-19 09:26:51 +0700

    Check if cell has top neighbor
    Signed-off-by: Rommy Gustiawan <rgcppp@gmail.com>

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,15 +13,21 @@ public class Main {
         for (int row = 0; row < grid.length; row++) {
             for (int column = 0; column < grid.length; column++) {
                 System.out.println(grid[row][column]);
+
                 boolean cellHasTopLeftNeighbor = false;
-                // if current index: row = 0, column = 0
-                // top left neighbor index must be: row = -1, column = -1
-                int topLeftNeighborRow = row - 1;
-                int topLeftNeighborColumn = column - 1;
-                if (topLeftNeighborRow >= 0 && topLeftNeighborColumn >= 0) {
+                int cellTopLeftNeighborRow = row - 1;
+                int cellTopLeftNeighborColumn = column - 1;
+                if (cellTopLeftNeighborRow >= 0 && cellTopLeftNeighborColumn >= 0) {
                     cellHasTopLeftNeighbor = true;
                 }
                 System.out.println("Cell has top left neighbor: " + cellHasTopLeftNeighbor);
+
+                boolean cellHasTopNeighbor = false;
+                int cellTopNeighborRow = row - 1;
+                if (cellTopNeighborRow >= 0) {
+                    cellHasTopNeighbor = true;
+                }
+                System.out.println("Cell has top neighbor: " + cellHasTopNeighbor);
             }
             System.out.println();
         }
